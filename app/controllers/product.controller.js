700c39ce672ef6c7a3f559f55c4c85c44839a72b
luisfucros <40307832+luisfucros@users.noreply.github.com>
2024-09-15 15:45:32 +0000

    update file

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -1,49 +1,50 @@
-const Product = require("../models/product.model");
+const Product = require("../models/product.model")
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 const getProducts = asyncWrapper(async (req, res) => {
     const products = await Product.find({})
     res.status(200).json(products)
 })
 
-const getProduct = asyncWrapper(async (req, res) => {
+const getProduct = asyncWrapper(async (req, res, next) => {
 
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id)
     if (!product) {
-      return res.status(404).json({ msg: `No product with id : ${id}` })
+      return next(createCustomError(`No product with id : ${id}`, 404))
     }
-    res.status(200).json(product);
+    res.status(200).json(product)
 
 });
 
 const createProduct = asyncWrapper(async (req, res) => {
-    const product = await Product.create(req.body);
+    const product = await Product.create(req.body)
     res.status(200).json(product);
 })
 
-const updateProduct = asyncWrapper(async (req, res) => {
+const updateProduct = asyncWrapper(async (req, res, next) => {
     const { id } = req.params;
 
-    const product = await Product.findByIdAndUpdate(id, req.body);
+    const product = await Product.findByIdAndUpdate(id, req.body)
 
     if (!product) {
-      return res.status(404).json({ message: `No product with id : ${id}` });
+      return next(createCustomError(`No product with id : ${id}`, 404))
     }
 
-    const updatedProduct = await Product.findById(id);
+    const updatedProduct = await Product.findById(id)
     res.status(200).json(updatedProduct);
 })
 
-const deleteProduct = asyncWrapper(async (req, res) => {
+const deleteProduct = asyncWrapper(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id);
 
     if (!product) {
-      return res.status(404).json({ message: `No product with id : ${id}` });
+      return next(createCustomError(`No product with id : ${id}`, 404))
     }
 
-    res.status(200).json({ message: "Product deleted successfully" });
+    res.status(200).json({ message: "Product deleted successfully" })
 })
 
 module.exports = {
--- app/index.js
+++ app/index.js
@@ -1,37 +1,37 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const connectDB = require("./db/connect.js");
 const app = express();
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler');
 require("dotenv").config();
 
 
 // middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // routes
-app.use("/api/products", productRoute);
+app.use("/api/v1/products", productRoute)
 
-app.use(notFound)
+app.use(notFound);
+app.use(errorHandlerMiddleware)
 
 app.get("/", (req, res) => {
-  res.json({status: 'ok'});
+  res.json({status: 'ok'})
 });
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(3000, () => {
-        console.log(`server is listening on port ${port}`);
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+        console.log(`server is listening on port ${port}`)
     })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-start();  
+start()
