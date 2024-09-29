d2c09223cf934f7a609293b221d362bd9bd3fa63
luisfucros <40307832+luisfucros@users.noreply.github.com>
2024-09-29 19:08:45 +0000

    user control

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,14 +1,26 @@
 const jwt = require('jsonwebtoken')
 const { BadRequestError } = require('../errors')
+const User = require('../models/user.model')
+const bcrypt = require('bcryptjs')
 
 const login = async(req, res) => {
-    const {username, password} = req.body
+  const {username, password} = req.body
 
-    if (!username || !password) {
-        throw new BadRequestError("Please provide email and password")
-    }
-    //just for demo, normally provided by DB!!!!
-  const id = new Date().getDate()
+  if (!username || !password) {
+      throw new BadRequestError("Please provide email and password")
+  }
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).json({ error: 'Invalid credentials' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ error: 'Invalid credentials' });
+  }
+
+  const id = user._id
 
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: '30d',
