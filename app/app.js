b3aac30a43723611da76c3c86a799a0ba62bc713
luisfucros <40307832+luisfucros@users.noreply.github.com>
2024-09-29 19:11:30 +0000

    add users

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
-const express = require("express");
-require('express-async-errors');
-const productRoute = require("./routes/product.route.js");
-const authRouter = require('./routes/auth.route.js');
-const connectDB = require("./db/connect.js");
-const app = express();
+const express = require("express")
+require('express-async-errors')
+const productRoute = require("./routes/product.route.js")
+const authRoute = require('./routes/auth.route.js')
+const userRoute = require('./routes/user.route.js')
+const connectDB = require("./db/connect.js")
+const app = express()
 const notFound = require('./middleware/not-found.js')
-const errorHandlerMiddleware = require('./middleware/error-handler.js');
-require("dotenv").config();
+const errorHandlerMiddleware = require('./middleware/error-handler.js')
+require("dotenv").config()
 
 
 // middleware
@@ -18,10 +19,11 @@ app.get("/", (req, res) => {
 });
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use("/api/v1/products", productRoute)
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/products', productRoute)
+app.use('/api/v1/users', userRoute)
 
-app.use(notFound);
+app.use(notFound)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000
