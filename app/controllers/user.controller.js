d16197b59aa6adea62e37ef98afe9de075b7a703
luisfucros <40307832+luisfucros@users.noreply.github.com>
2024-10-01 00:11:42 +0000

    update file

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,11 +2,6 @@ const User = require('../models/user.model');
 
 const createUser = async (req, res) => {
     const { username, password } = req.body;
-  
-    // const { error } = userSchema.validate({ username, password });
-    // if (error) {
-    //   return res.status(400).json({ error: error.details[0].message });
-    // }
 
     const existingUser = await User.findOne({ username });
     if (existingUser) {
